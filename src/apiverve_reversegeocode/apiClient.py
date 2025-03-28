8997805507e37a4ffbd551493f145914d23455dd
APIVerve Team <hello@apiverve.com>
2025-03-28 17:46:47 -0500

    Bug Fixes

--- src/apiverve_reversegeocode/apiClient.py
+++ src/apiverve_reversegeocode/apiClient.py
@@ -10,10 +10,10 @@ class ReversegeocodeAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/reversegeocode'
+        self.base_url = 'https://api.apiverve.com/v1/reversegeocode'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
